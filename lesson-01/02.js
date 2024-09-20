32971f8c91499e849cb94438b01272bc29bc73d1
GudkovSergey <gudkow-sergey@mail.ru>
2024-09-20 19:07:40 +0500

    02.01

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,5 @@
 let number1 = Math.floor(Math.random() * 100);
-if(number1 %2 === 0){
-    console.log(`"${number1} :это четное число"`);
-}else{
-        console.log(`"${number1} :это нечетное число"`);  
-    };
+
+(number1 %2 === 0)?console.log(`"${number1}: это четное число"`):console.log(`"${number1}: это нечетное число"`);
+
 
