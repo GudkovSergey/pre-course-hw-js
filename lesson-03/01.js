d92809b848fa59d484993e1f17c20577a922ac18
GudkovSergey <gudkow-sergey@mail.ru>
2024-10-04 16:15:12 +0500

    03.01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const result = colors.filter(i=>i==="черный"|| i==="красный"|| i==="желтый").join("-");
+    
+    return result
+}
+
+console.log(createColorString());
+
